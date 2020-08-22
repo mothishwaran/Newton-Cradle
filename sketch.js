a180a85a88dcdfd4c6b54a54efbe1d6dcc1ee4b5
mothishwaran <68004760+mothishwaran@users.noreply.github.com>
2020-08-22 05:30:19 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -77,7 +77,7 @@ function keyPressed() {
 
 }
 function isTouching(){
-  if(bob1.Collide(bob2)){
+  if(bob1.isTouching(bob2)){
 
     Matter.Body.applyForce(bob5.body,bob5.body.position,{x:100,y:0})
   }
